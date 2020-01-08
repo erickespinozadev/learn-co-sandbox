d4f2079e7effcbc3bb89b4932b0dba297914e7a7
Erick Espinoza <erickespinoza.dev@icloud.com>
2020-01-08 21:09:39 +0000

    Automatically backed up by Learn

--- example.rb
+++ example.rb
@@ -2,3 +2,10 @@ helper_method
 puts 3+7= validates_numericality_of
 17 for authenticate_or_request_with_http_digest
 (1+sum)
+around_filter
+stylesheet_link_tag
+hidden_field_tag
+(1+sum of x equals)
+the sum of x plus the sum of 1 equals 
+authenticate_or_request_with_http_digest
+the RUBY_RELEASE_DATE
